a2ccdeaee454d4b2029252a648571b42674d45e6
SamsoniteSwan <swanson_jeremye@yahoo.com>
2016-02-25 18:54:02 -0500

    uploaded sql and csv files

---
+++ src/main/sql/DBInitialization_Payroll.sql
@@ -0,0 +1,62 @@
+/* TODO - create payroll database */
+/* Clean database tables */
+DROP TABLE IF EXISTS payroll.tblPayPeriods CASCADE;
+DROP TABLE IF EXISTS payroll.tblWorkLog CASCADE;
+DROP TABLE IF EXISTS payroll.tblEmployees CASCADE;
+DROP TABLE IF EXISTS payroll.tblRecords CASCADE;
+
+
+
+CREATE TABLE payroll.tblEmployees(
+   employeeId VARCHAR(10) NOT NULL,
+   firstName VARCHAR(100) NOT NULL,
+   lastName VARCHAR(100) NOT NULL,
+   dob DATETIME,
+   status VARCHAR(20) NOT NULL DEFAULT 'active',
+   PRIMARY KEY (employeeId)
+);
+
+/* daily hours. Foreign key from employeeID, or payPeriod ID (or both)? */
+CREATE TABLE payroll.tblWorkLog(
+   id INT NOT NULL AUTO_INCREMENT,
+   workday DATETIME NOT NULL,
+   employee VARCHAR(10) NOT NULL,
+   hours DECIMAL NOT NULL,
+   vacationhrs DECIMAL NOT NULL,
+   FOREIGN KEY (employee) REFERENCES tblEmployees(employeeId),
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE payroll.tblPayPeriods(
+   id INT NOT NULL AUTO_INCREMENT,
+   wage DECIMAL NOT NULL,
+   startDate DATETIME NOT NULL,
+   endDate DATETIME NOT NULL,
+   employee VARCHAR(10) NOT NULL,
+   FOREIGN KEY (employee) REFERENCES tblEmployees(employeeId),
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE payroll.tblRecords(
+   recordId INT NOT NULL AUTO_INCREMENT,
+   employeeId VARCHAR(10) NOT NULL,
+   employeeFN VARCHAR(100),
+   employeeLN VARCHAR(100),
+   period VARCHAR(50) NOT NULL,
+   PRIMARY KEY (recordId)
+);
+
+INSERT INTO payroll.tblEmployees (employeeId, lastName, firstName, dob) VALUES ("0000", "Smith", "Joe", '1999/10/10');
+INSERT INTO payroll.tblEmployees (employeeId, lastName, firstName, dob) VALUES ("1111", "Stevensen", "Steve", '1986/4/18');
+
+INSERT INTO payroll.tblWorkLog (employee, workday, hours, vacationhrs) VALUES ("0000", '2015/10/10', '5.5', '3.0');
+INSERT INTO payroll.tblWorkLog (employee, workday, hours, vacationhrs) VALUES ("0000", '2015/10/11', '8.0', '0.0');
+INSERT INTO payroll.tblWorkLog (employee, workday, hours, vacationhrs) VALUES ("1111", '2015/10/10', '7.5', '0.0');
+
+INSERT INTO payroll.tblPayPeriods (employee, startDate, endDate, wage) VALUES ("0000", '2015/10/10', '2015/10/17', '35.20');
+INSERT INTO payroll.tblPayPeriods (employee, startDate, endDate, wage) VALUES ("0000", '2015/10/18', '2015/10/25', '37.65');
+
+INSERT INTO payroll.tblRecords (employeeId, employeeFN, employeeLN, period) VALUES ('0000', 'Joe', 'Schmoe', '1/15/2015-1/29/2015');
+INSERT INTO payroll.tblRecords (employeeId, employeeFN, employeeLN, period) VALUES ('1111', 'Thelma', 'Louise', '1/30/2015-2/13/2015');
+
+
